5f4caee08b886aa0c7b05b60416ddaafdeb7bbb2
shobit1337 <shobitdeshwal1337@gmail.com>
2022-04-26 03:31:07 +0530

    feat: eslint prettier husky setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-  return <h1 class="text-3xl font-bold underline"> Hello world! </h1>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
